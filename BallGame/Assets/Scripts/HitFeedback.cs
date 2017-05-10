3886bbd14f3dd805f442e081cbd6c6bb886a7fab
RytmeAnders <aschristiansen@gmail.com>
2017-05-10 16:00:10 +0200

    Began score synthesis

---
+++ BallGame/Assets/Scripts/HitFeedback.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitFeedback : MonoBehaviour {
+
+    [Range(0, 100)] public float score;
+    AudioSource ding;
+
+    void Start () {
+        ding = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+        ding.pitch = .1F + (score/100);
+	}
+}
